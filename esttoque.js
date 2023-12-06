7f8d2db7c8488be96ed038d7d7a04c58b16d465a
Breno Machado <breno.machado@estudante.ufjf.br>
2023-12-06 20:19:44 -0300

    primeira parte da aula

---
+++ esttoque.js
@@ -0,0 +1,16 @@
+
+let estoque = {
+
+'joao' :  [{'tipo' : 'maca', 'qtd' : 1},
+            {'tipo' : 'pera', 'qtd' : 1}
+],
+'maria' : [{'tipo' : 'maca' , 'qtd' : 2},
+            {'tipo' : 'banana', 'qtd' : 1}
+],
+
+};
+
+function getEstoque(){
+    return structuredClone(estoque);
+}
+
