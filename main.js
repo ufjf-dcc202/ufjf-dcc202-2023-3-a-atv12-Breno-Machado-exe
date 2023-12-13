6f344a9ee0c8306f1a43f67791341ef2df28010c
afgfgfg <jfhdjslsjdjf>
2023-12-13 20:17:19 -0300

    atualiza

--- main.js
+++ main.js
@@ -25,20 +25,16 @@ function leFormulario (event) {
 
 function atualizaTela() {
     const estoque = getEstoque();
-    const joao = estoque['joao'];
-    olJoao.innerHTML = " ";
-    for( let i=0 ; i<joao.length; i++){
-        const monte = joao[i];
+    preencheListaPessoa(estoque['joao'], olJoao);
+    preencheListaPessoa(estoque['maria'], olMaria);
+}
+
+function preencheListaPessoa(pessoa, lista){
+    lista.innerHTML = " ";
+    for( let i=0 ; i<pessoa.length; i++){
+        const monte = pessoa[i];
         const eli = document.createElement('li');
         eli.innerText = `${monte.tipo}: ${monte.qtd}`;
-        olJoao.append(eli);
-    }
-    const maria = estoque['maria'];
-    olMaria.innerHTML = " ";
-    for( let i=0 ; i<maria.length; i++){
-        const monte = maria[i];
-        const eli = document.createElement('li');
-        eli.innerText = `${monte.tipo}: ${monte.qtd}`;
-        olMaria.append(eli);
+        lista.append(eli);
     }
 }
