72d7764e5f11ec646879bd8ab0d97d7dce3eea9d
Breno Machado <breno.machado@estudante.ufjf.br>
2023-12-14 13:59:49 -0300

    atualiza o main.js

--- main.js
+++ main.js
@@ -1,40 +1,62 @@
-import { getEstoque, transacao } from "./estoque.js";
+
+import { getEstoque, limpaEstoque, transacaoNoEstoque } from "./estoque.js";
+
+document.entrada.addEventListener('submit', leFormulario);
 
 
-document.entrada.addEventListener('submit',leFormulario);
-//leFormulario();
-
-const olJoao = document.querySelector("#olJoao");
-const olMaria = document.querySelector("#olMaria");
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
 
 atualizaTela();
 
-function leFormulario (event) {
+function leFormulario(event)
+{
     event.preventDefault();
+    
     const fruta = document.entrada.fruta.value;
     const quantidade = document.entrada.quantidade.valueAsNumber;
     const origem = document.entrada.origem.value;
     const destino = document.entrada.destino.value;
-
+    
     console.log(`solicitado: ${origem} doa ${quantidade} ${fruta} para ${destino}`);
-    //document.entrada.submit();
 
-    transacao(origem, destino, quantidade, fruta);
+
+    transacaoNoEstoque(origem, destino, fruta, quantidade);
     atualizaTela();
 }
 
-function atualizaTela() {
+document.addEventListener('DOMContentLoaded', () => 
+{
+    document.getElementById('botaoLimparLista').addEventListener('click', () => 
+    {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
+
+function atualizaTela() 
+{
     const estoque = getEstoque();
+    
+    document.entrada.fruta.value = "maca";
+    document.entrada.quantidade.value = 1;
+   
     preencheListaPessoa(estoque['joao'], olJoao);
     preencheListaPessoa(estoque['maria'], olMaria);
 }
 
-function preencheListaPessoa(pessoa, lista){
-    lista.innerHTML = " ";
-    for( let i=0 ; i<pessoa.length; i++){
-        const monte = pessoa[i];
-        const eli = document.createElement('li');
-        eli.innerText = `${monte.tipo}: ${monte.qtd}`;
-        lista.append(eli);
+function preencheListaPessoa(pessoa, lista)
+{
+    lista.innerHTML = "";
+
+    if(Array.isArray(pessoa)) 
+    {
+        for(let i = 0; i < pessoa.length; i++) 
+        {
+            const monte = pessoa[i];
+            const eLi = document.createElement('li');
+            eLi.innerText = `${monte.tipo}: ${monte.quantidade}`;
+            lista.append(eLi);
+        }
     }
 }
